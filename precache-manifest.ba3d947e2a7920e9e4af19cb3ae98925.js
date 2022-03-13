00104fbfaf0d2de8153cf8be231c157080ef3689
Code Sandbox <hello@codesandbox.io>
2022-03-13 09:44:50 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.ba3d947e2a7920e9e4af19cb3ae98925.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac7e412040a87f9b59a856f35eb6f7f",
+    "url": "https://afzalsulemani92.github.io/csb-l6mso5/index.html"
+  },
+  {
+    "revision": "bcf2a5f21878e7a4bb72",
+    "url": "https://afzalsulemani92.github.io/csb-l6mso5/static/js/2.7bd492ba.chunk.js"
+  },
+  {
+    "revision": "34c84a8ba27ea8361f78",
+    "url": "https://afzalsulemani92.github.io/csb-l6mso5/static/js/main.1a1a733a.chunk.js"
+  },
+  {
+    "revision": "fdec7a486967c0ee2d0f",
+    "url": "https://afzalsulemani92.github.io/csb-l6mso5/static/js/runtime-main.0a414a28.js"
+  }
+]);
